3a7261df40d32f330172b0f249180fd88b396e85
qwr133 <ksg165000@gmail.com>
2023-04-16 16:05:41 +0900

    switch 때문에 하는 commit

--- src/Wine_yejin/main.java
+++ src/Wine_yejin/main.java
@@ -1,14 +1,21 @@
 package Wine_yejin;
 
-
-
-public class main {
+public class Main {
     public static void main(String[] args) {
         MainView mainView = new MainView();
+        MainView.start();
 
-        mainView.start();
-//        TypeSelect typeSelect = new TypeSelect();
-        TotalUserRepository total = new TotalUserRepository();
+
+        TotalUserRepository totalUserRepository = new TotalUserRepository();
+        Employ employ = new Employ();
+
+//        List<Employ> = List.of(
+//          new Employ("홍길동",100.0,"aaaa","1111","영업"),
+//          new Employ("최수미",100.0,"bbbb","1111","영업"),
+//          new Employ("롤롤롤",100.0,"cccc","1111","영업")
+//        );
+
+
 
 
     }
